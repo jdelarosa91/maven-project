521f5d2dea855e41f6786906a65c67ac2f26987a
Javier de la Rosa <javier.rf91@gmail.com>
2018-05-21 18:32:07 -0500

    Fixing styles

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
   /**
-   * This is a method
-   * @param someone Someone's name
-   * @return String saying hello to someone
+   * This is a method.
+   * @param someone Someone's name.
+   * @return String saying hello to someone.
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
